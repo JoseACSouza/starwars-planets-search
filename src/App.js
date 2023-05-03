b4139bfef93d0eec63612a63a964f20470c25a7b
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-05-03 16:13:41 -0300

    cumpre requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [info, setInfo] = useState([]);
+  useEffect(
+    () => {
+      async function fetchData() {
+        const data = await fetch('https://swapi.dev/api/planets');
+        const results = await data.json();
+        results.results.forEach((element) => {
+          delete element.residents;
+        });
+        setIsLoading(false);
+        setInfo(results.results);
+      }
+      fetchData();
+    },
+    [],
+  );
   return (
-    <span>Hello, App!</span>
+    <div>
+      {isLoading ? <p>Carregando...</p> : (
+        <table>
+          <thead>
+            <tr>
+              {Object.keys(info[0]).map((key) => (
+                <th key={ key }>
+                  { key }
+                </th>))}
+            </tr>
+          </thead>
+          <tbody>
+            { info.map((item) => (
+              <tr key={ item.name }>
+                {Object.keys(item).map((keyValue, index) => (
+                  <td key={ index }>
+                    { item[keyValue] }
+                  </td>
+                ))}
+              </tr>
+            )) }
+          </tbody>
+        </table>
+      )}
+    </div>
   );
 }
 
